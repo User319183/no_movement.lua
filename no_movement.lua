6186c0c20095cd694034add69cf45f4cb1a0b089
Sathvik <88944590+User319183@users.noreply.github.com>
2022-09-02 16:27:49 -0400

    Update and rename no_speed.lua to no_movement.lua

--- no_movement.lua
+++ no_movement.lua
@@ -3,3 +3,6 @@ local player = game.Players.LocalPlayer
 --set the player's speed to 0
 player.Character.Humanoid.WalkSpeed = 0
     
+
+--set the player's jump power to 0
+player.Character.Humanoid.JumpPower = 0
